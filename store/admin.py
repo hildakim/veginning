ce4d45a8604c8d8680f315509473fa90b17b1def
unun2 <jaeun101501@gmail.com>
2021-07-01 19:58:24 +0900

    2commit

--- store/admin.py
+++ store/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Store
+from .models import Store2
 
 admin.site.register(Store)
+admin.site.register(Store2)
--- store/models.py
+++ store/models.py
@@ -14,4 +14,32 @@ class Store(models.Model):
         return self.title
 
     def summary(self):
-        return self.body[:30]
+        return self.body[:20]
+        
+class Store2(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    body = models.TextField()
+    body2=models.TextField()
+    image = models.ImageField(upload_to="store/", blank=True, null=True)
+    thumbnail = ImageSpecField(source = 'image', processors = [ResizeToFill(200,300)], format = "JPEG")
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
+
+class Store3(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    body = models.TextField()
+    body2=models.TextField()
+    image = models.ImageField(upload_to="store/", blank=True, null=True)
+    thumbnail = ImageSpecField(source = 'image', processors = [ResizeToFill(200,300)], format = "JPEG")
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:20]
