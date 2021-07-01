ca5785b381d043d82125a6afba24977574e24673
hyeinjeon <hayleyjhi@naver.com>
2021-07-01 18:00:30 +0900

    commit 4

--- veginning/settings.py
+++ veginning/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'veginning.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'veginning/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
