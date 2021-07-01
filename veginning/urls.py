1f5be18d38bb98f622883d23ae5f52fc185f5035
hildakim <pinkredjohn@gmail.com>
2021-07-01 17:54:58 +0900

    Update urls.py

--- veginning/urls.py
+++ veginning/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from main.view import home
+from main.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
